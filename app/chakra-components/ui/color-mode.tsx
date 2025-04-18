4f7c921f33ef66556be4a30119a96735c557cc2a
Matt Macdonald <matt.david.macdonald@gmail.com>
2025-04-18 16:03:05 -0700

    commenting out issues

--- app/chakra-components/ui/color-mode.tsx
+++ app/chakra-components/ui/color-mode.tsx
@@ -7,7 +7,8 @@ import { ThemeProvider, useTheme } from 'next-themes';
 import * as React from 'react';
 import { LuMoon, LuSun } from 'react-icons/lu';
 
-export interface ColorModeProviderProps extends ThemeProviderProps {}
+export type ColorModeProviderProps = ThemeProviderProps;
+// export interface ColorModeProviderProps extends ThemeProviderProps {}
 
 export function ColorModeProvider(props: ColorModeProviderProps) {
     return <ThemeProvider attribute={'class'} disableTransitionOnChange {...props} />;
@@ -43,7 +44,8 @@ export function ColorModeIcon() {
     return colorMode === 'dark' ? <LuMoon /> : <LuSun />;
 }
 
-interface ColorModeButtonProps extends Omit<IconButtonProps, 'aria-label'> {}
+type ColorModeButtonProps = Omit<IconButtonProps, 'aria-label'>;
+// interface ColorModeButtonProps extends Omit<IconButtonProps, 'aria-label'> {}
 
 export const ColorModeButton = React.forwardRef<HTMLButtonElement, ColorModeButtonProps>(
     function ColorModeButton(props, ref) {
