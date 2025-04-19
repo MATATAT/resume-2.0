7b21472f28900d17bc3ef9157c41c93eb306b1c8
Matt Macdonald <matt.david.macdonald@gmail.com>
2025-04-19 09:25:11 -0700

    Basic main experience

--- app/routes/_home._index/Main.tsx
+++ app/routes/_home._index/Main.tsx
@@ -1,10 +1,31 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Heading, List } from '@chakra-ui/react';
 
 export const Main = () => {
     return (
         <Box as={'main'}>
-            {/* Main content area */}
-            <h1>Welcome to the main content area!</h1>
+            <Heading as={'h3'} fontWeight={'bold'} textTransform={'uppercase'} pl={[0, 3]}>
+                Work Experience
+            </Heading>
+            <Box pl={[4, 8]}>
+                <ExperienceBlock />
+            </Box>
+        </Box>
+    );
+};
+
+const ExperienceBlock = () => {
+    return (
+        <Box>
+            <Box fontWeight={'bold'}>Software Engineer</Box>
+            <Box>Amazon</Box>
+            <Box>Seattle, WA</Box>
+            {/* {experience.startDate} - {experience.endDate || 'Current'} / {experience.location} */}
+            <Box>2021 - Current</Box>
+            <List.Root pl={8}>
+                <List.Item>Worked on the Amazon Prime Video team.</List.Item>
+                <List.Item>Worked on the Amazon Music team.</List.Item>
+                <List.Item>Worked on the Amazon Alexa team.</List.Item>
+            </List.Root>
         </Box>
     );
 };
