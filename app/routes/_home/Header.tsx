1e9e07eaf265fb88cdb4d0b48ba58b7d86eacacb
Matt Macdonald <matt.david.macdonald@gmail.com>
2025-04-21 22:11:10 -0700

    good enough

--- app/routes/_home/Header.tsx
+++ app/routes/_home/Header.tsx
@@ -1,17 +1,43 @@
-import { Heading, VStack } from '@chakra-ui/react';
+import { Box, Heading, IconButton, VStack } from '@chakra-ui/react';
+import { FiPrinter } from 'react-icons/fi';
 import { useResume } from '~/context/ResumeContext';
 
 export const Header = () => {
     const { name, title } = useResume();
 
+    const handlePrint = () => {
+        window.print();
+    };
+
     return (
-        <VStack gap={2} py={8} borderBottom={3} borderStyle={'solid'} borderColor={'gray.700'} px={4} data-print-border>
-            <Heading as={'h1'} size={'6xl'} textAlign={'center'} textTransform={'uppercase'} color={'white'}>
-                {name}
-            </Heading>
-            <Heading as={'h2'} size={'xl'} color={'teal.400'} textTransform={'uppercase'} fontStyle={'italic'}>
-                {title}
-            </Heading>
-        </VStack>
+        <Box position='relative'>
+            <IconButton
+                variant={'surface'}
+                aria-label='Print resume'
+                position='absolute'
+                right={4}
+                top={4}
+                onClick={handlePrint}
+                data-print-hide
+            >
+                <FiPrinter />
+            </IconButton>
+            <VStack
+                gap={2}
+                py={8}
+                borderBottom={3}
+                borderStyle={'solid'}
+                borderColor={'gray.700'}
+                px={4}
+                data-print-border
+            >
+                <Heading as={'h1'} size={'6xl'} textAlign={'center'} textTransform={'uppercase'} color={'white'}>
+                    {name}
+                </Heading>
+                <Heading as={'h2'} size={'xl'} color={'teal.400'} textTransform={'uppercase'} fontStyle={'italic'}>
+                    {title}
+                </Heading>
+            </VStack>
+        </Box>
     );
 };
